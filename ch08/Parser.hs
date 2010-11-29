636b6412891894577715bd13a45d570b19d95c47
Tianyi Cui <tianyicui@gmail.com>
2010-11-29 18:07:21 +0800

    why doesn't this compile with GHC?

--- ch08/Parser.hs
+++ ch08/Parser.hs
@@ -1,4 +1,6 @@
-import Prelude hiding (return)
+module Parser where
+
+import Prelude (Char, String)
 
 type Parser a = String -> [(a, String)]
 
@@ -15,3 +17,23 @@ item = \inp -> case inp of
 
 parse :: Parser a -> String -> [(a, String)]
 parse p inp = p inp
+
+(>>=) :: Parser a -> (a -> Parser b) -> Parser b
+p >>= f = \inp -> case parse p inp of
+                       [] -> []
+                       [(v,out)] -> parse (f v) out
+
+p0 :: Parser (Char, Char)
+p0 =
+    item >>= \x ->
+    item >>= \_ ->
+    item >>= \y ->
+    return (x, y)
+
+-- is equivalent to
+-- p1 :: Parser (Char, Char)
+p1 = do
+    x <- item
+    item
+    y <- item
+    return (x, y)
